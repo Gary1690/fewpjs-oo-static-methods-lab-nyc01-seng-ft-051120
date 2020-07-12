3284ee34ec696eb0c88ca22608e5c00291e02ad5
Gary Cordero <GaryCordero1690@gmail.com>
2020-07-12 16:12:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    
+    return (string[0].toUpperCase() + string.substr(1))
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string){
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result =  string.split(" ").map(word =>{
+        if(exceptions.includes(word)){
+          return word
+        }
+        return this.capitalize(word)
+    }).join(" ")
+
+    return this.capitalize(result)
+  }
 }
